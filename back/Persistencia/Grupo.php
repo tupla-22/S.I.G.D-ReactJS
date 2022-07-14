e9ee9bb36464191db3a6751ca13b4fe482b9da02
Ivan-FF <forteferraroivan@gmail.com>
2022-07-14 16:35:23 -0300

    Create Grupo.php

---
+++ back/Persistencia/Grupo.php
@@ -0,0 +1,58 @@
+<?php 
+
+class Grupo{
+
+    private $id;
+    private $nombre;
+
+    function __construct(
+        $id,
+        $nombre
+    )
+    {
+        $this->id=$id;
+        $this->nombre=$nombre;
+    }
+
+    /**
+     * Get the value of id
+     */ 
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set the value of id
+     *
+     * @return  self
+     */ 
+    public function setId($id)
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+
+    /**
+     * Get the value of nombre
+     */ 
+    public function getNombre()
+    {
+        return $this->nombre;
+    }
+
+    /**
+     * Set the value of nombre
+     *
+     * @return  self
+     */ 
+    public function setNombre($nombre)
+    {
+        $this->nombre = $nombre;
+
+        return $this;
+    }
+}
+
+?>
