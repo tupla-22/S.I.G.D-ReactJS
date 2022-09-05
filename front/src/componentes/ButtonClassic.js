f3c9a04e071962b2b28b0e22251d00c5dad95d95
Lucas Pérez <86010222+ElCoda33@users.noreply.github.com>
2022-09-05 12:09:57 -0300

    Reparacion de formulario de login

--- front/src/componentes/ButtonClassic.js
+++ front/src/componentes/ButtonClassic.js
@@ -3,9 +3,10 @@ import { Button, styled } from "@mui/material";
 
 
 export const ButtonClassic = styled(Button)(({ theme }) => ({
+  color:"#fff",
   backgroundColor: "#fb6900",
   '&:hover': {
-    backgroundColor: "#fb6900",
+    backgroundColor: "#fb690099",
   },
 }));
 
--- front/src/componentes/FormLogin.js
+++ front/src/componentes/FormLogin.js
@@ -6,23 +6,59 @@ import Link from './Link';
 import "./styles/FormLogin.css"
 import { useState } from 'react';
 import RecoverPassword from './RecoverPassword';
+import { helpHttp } from '../helpers/helpHttp';
+import { PAlert } from './PAlert';
 
 const FormLogin = () => {
-  const [errors, setErrors] = useState({vacio:true});
-  const [usuario, setUsuario] = useState("");
+  const [errors, setErrors] = useState({errors:false,correct:false});
+  const [usuario, setUsuario] = useState({password_usuario:null,ci_usuario:null});
   const navigate = useNavigate();
 
   const regexUsuario =/^([0-9]){1,12}$/;
 
+
+
   const handleSubmit = (e) =>{
-    if(!errors.usuario!="" && errors.vacio!=true){
-      navigate(`/student/${Math.round(Math.random()*1000)}/homeStudent`);
+    const data = new URLSearchParams(usuario);
+
+    const options ={
+      method:"POST",
+      headers:{"Content-type": "application/x-www-form-urlencoded;charset-UTF-8"},
+      body:data
+    }
+    if(!errors.errors){
+
+      const getUser = async ()=>{
+        const resp = await fetch("http://apirest.com/usuarios?login=true&suffix=usuario",options).then(e=>e.json()).then(e=>{
+
+          
+        if(e.status==200){
+          setErrors({...errors,correct:false});
+          navigate(`/student/${e.result[0].ci_usuario}/homeStudent`);
+
+          
+        }else setErrors({...errors,correct:true});
+
+        })
+        
+        
+        
+        
+
+        
+
+        
+        // console.log(user)
+      }
+      getUser();
+
+      
     }
   }
 
 
   const handleBlur = (e) =>{
-    if(!regexUsuario.test(usuario.trim())){
+    if(!regexUsuario.test(usuario.ci_usuario.trim())){
       setErrors({...errors,
         usuario:"El campo cédula solo acepta números y hasta 8 caracteres",
         vacio:true
@@ -33,20 +69,26 @@ const FormLogin = () => {
     });}
   }
   const handleChange = (e) =>{ 
-    setUsuario(e.target.value);
+    setUsuario({...usuario,[e.target.name]:e.target.value});
+    console.log(usuario)
   } 
 
     return ( 
         <div className="formLogin">
+          {errors.correct && <PAlert>Cédula o contraseña incorrecta</PAlert>}
         <TextField
+          name='ci_usuario'
           className="formLogin__input"
           label="Cédula"
           variant='outlined'
           onChange={handleChange}
           onBlur={handleBlur}
         />
-        {errors.usuario && <div style={{color:"red"}}>{errors.usuario}</div>}
+        {errors.usuario &&  <PAlert>{errors.usuario}</PAlert>}
         <TextField
+        
+          onChange={handleChange}
+          name='password_usuario'
           className="formLogin__input"
           label="Contraseña"
           variant='outlined'
