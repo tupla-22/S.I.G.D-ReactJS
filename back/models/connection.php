792b2e19e4cb5edfc2308b6f3fea51539c2452c2
Ivan-FF <forteferraroivan@gmail.com>
2022-09-05 16:49:07 -0300

    Update connection.php

--- back/models/connection.php
+++ back/models/connection.php
@@ -91,7 +91,7 @@ class Connection{
 
     static public function jwt($id,$ci){
         //pido el rol del usuario
-        $rol= GetModel::getRelDataFilter(
+        /*$rol= GetModel::getRelDataFilter(
             $rel="roles,usuarios", 
             $type="rol,usuario", 
             $select="id_rol", 
@@ -99,7 +99,7 @@ class Connection{
             $equalTo=$id, $orderBy=null, 
             $orderMode=null, 
             $startAt=null, 
-            $endAt=null);
+            $endAt=null);*/
        
         $time= time();
 
