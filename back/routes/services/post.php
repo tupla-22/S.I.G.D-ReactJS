cb4becc18af3cc726736e532d1401126eaf25c88
Ivan-FF <forteferraroivan@gmail.com>
2022-10-03 16:54:11 -0300

    exepciones

--- back/routes/services/post.php
+++ back/routes/services/post.php
@@ -57,44 +57,79 @@ if (isset($_POST)) {
         /**===================peticion post para usuarios autorizados======================= */
         if(isset($_GET["token"])){
 
-            $tableToken= $_GET["table"] ?? "usuarios";
-            $suffix= $_GET["suffix"] ?? "usuario";
+            /**peticion put para usuarios no autorizados */
 
-            $validate= Connection::tokenValidate($_GET["token"],$tableToken, $suffix);
+            if($_GET["token"]=="no" && isset($_GET["exept"])){
 
-            /**=======================
-             * solicitamos respuesta del controlador para crear datos en cualquier tabla
-             * ========================= */
+                //-----------validar la tabla y las columnas existan--------------
 
-            if ($validate == "ok") {
+                $columns= array($_GET["exept"]);
+
+                if(empty(Connection::getColumnData($table, $columns))){
+
+                    $json= array(
                 
-
-                
-                $response-> postData($table, $_POST);
-            }
-
-            //error cuando el token ha expirado
-
-            if ($validate == "expired") {
-                $json= array(
-                    'status' => 303,
-                    'results' => "Error: the token was expired"
-                );
-                echo json_encode($json, http_response_code($json["status"]));
-                return;
-            }
-
-            //error cuando el token no coincide en bd
-
-            if ($validate == "no-auth") {
-                $json= array(
-                    'status' => 400,
-                    'results' => "Error: the user is not authorized"
-                );
-                echo json_encode($json, http_response_code($json["status"]));
-                return;
-            }
+                        "status" => 400,
+                        "result" =>"Error: Fields in the form do not match the database"
+                    
+                    
+                    );
             
+                    echo json_encode($json, http_response_code($json["status"]));
+            
+                    return;
+            
+                }
+
+                /**=======================
+                 * solicitamos respuesta del controlador para crear datos en cualquier tabla
+                 * ========================= */
+            
+                $response-> postData($table, $_POST);
+
+
+            /**peticion put para usuarios autorizados */
+
+            }else{
+
+                $tableToken= $_GET["table"] ?? "usuarios";
+                $suffix= $_GET["suffix"] ?? "usuario";
+
+                $validate= Connection::tokenValidate($_GET["token"],$tableToken, $suffix);
+
+                /**=======================
+                 * solicitamos respuesta del controlador para crear datos en cualquier tabla
+                 * ========================= */
+
+                if ($validate == "ok") {
+                    
+
+                    
+                    $response-> postData($table, $_POST);
+                }
+
+                //error cuando el token ha expirado
+
+                if ($validate == "expired") {
+                    $json= array(
+                        'status' => 303,
+                        'results' => "Error: the token was expired"
+                    );
+                    echo json_encode($json, http_response_code($json["status"]));
+                    return;
+                }
+
+                //error cuando el token no coincide en bd
+
+                if ($validate == "no-auth") {
+                    $json= array(
+                        'status' => 400,
+                        'results' => "Error: the user is not authorized"
+                    );
+                    echo json_encode($json, http_response_code($json["status"]));
+                    return;
+                }
+            }    
 
         //error cuando no envia el token
             
