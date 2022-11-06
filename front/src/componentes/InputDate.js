b8cea4a8d4c29b185ed4b42e636c10f2cb2d06cc
Lucas Pérez <86010222+ElCoda33@users.noreply.github.com>
2022-11-05 23:07:21 -0300

    VALIDACON de formularios

--- front/src/componentes/InputDate.js
+++ front/src/componentes/InputDate.js
@@ -1,37 +1,41 @@
-import * as React from "react";
-import TextField from "@mui/material/TextField";
-import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
-import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider";
-import { DatePicker } from "@mui/x-date-pickers/DatePicker";
-import { useEffect } from 'react';
+import * as React from "react"
+import TextField from "@mui/material/TextField"
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns"
+import { LocalizationProvider } from "@mui/x-date-pickers/LocalizationProvider"
+import { DatePicker } from "@mui/x-date-pickers/DatePicker"
+import { useEffect } from "react"
+import { FormControl, FormHelperText } from "@mui/material"
 
-const InputDate = ({ok, name, className, form, setForm,label }) => {
-  const [value, setValue] = React.useState(null);
+const InputDate = ({require,  ok, helperText, name, className, form, setForm, label }) => {
+	const [value, setValue] = React.useState(null)
 
-  useEffect(() => {
-    setValue(null)
-  }, [ok]);
-  const handleChange = (e) => {
-    setValue(e);
-    setForm({
-      ...form,
-      [name]: `${e.getFullYear()}-${e.getMonth() + 1}-${e.getDate()}`,
-    });
-  };
-  return (
-    <div style={{ margin: "20px 0", width: "100%" }}>
-        <LocalizationProvider dateAdapter={AdapterDateFns}>
+	useEffect(() => {
+		setValue(null)
+	}, [ok])
+	const handleChange = (e) => {
+		setValue(e)
+		setForm({
+			...form,
+			[name]: `${e.getFullYear()}-${e.getMonth() + 1}-${e.getDate()}`,
+		})
+	}
+	return (
+		<div style={{ margin: "20px 0", width: "100%" }}>
+			<FormControl >
+				<LocalizationProvider dateAdapter={AdapterDateFns}>
           <DatePicker
-            className="Form__input"
-            label={label}
-            value={value}
-            name={name}
-            onChange={handleChange}
-            renderInput={(params) => <TextField {...params} />}
-          />
-        </LocalizationProvider>
-        </div>
-  );
-};
+						className="Form__input"
+						label={label}
+						value={value}
+						name={name}
+						onChange={handleChange}
+						renderInput={(params) => <TextField {...params} />}
+					/>
+				</LocalizationProvider>
+				<FormHelperText>{helperText}</FormHelperText>
+			</FormControl>
+		</div>
+	)
+}
 
-export default InputDate;
+export default InputDate
--- front/src/hooks/useValidaterForm.js
+++ front/src/hooks/useValidaterForm.js
@@ -1,51 +1,19 @@
 import { useState } from "react";
 import { helpHttp } from "../helpers/helpHttp";
 
-export const useValidaterForm = (initialForm, validateForm) => {
-  const [form, setForm] = useState(initialForm);
-  const [errors, setErrors] = useState({});
-  const [loading, setLoading] = useState(false);
-  const [response, setResponse] = useState(null);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
+const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+	const ciRegex = /^(\d){4,8}/i
 
-    setForm({
-      ...form,
-      [name]: value,
-    });
-  };
+export const useValidaterForm = (form)=> {
+    const [errors, setErrors] = useState({});
 
-  const handleBlur = (e) => {
-    handleChange(e);
-    setErrors(validateForm(form));
-  };
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    setErrors(validateForm(form));
-
-    if (Object.keys(errors).length === 0) {
-      alert("Enviando Formulario");
-      setLoading(true);
-      helpHttp()
-        .post("https://formsubmit.co/ajax/jonmircha@gmail.com", {
-          body: form,
-          headers: {
-            "Content-Type": "application/json",
-            Accept: "application/json",
-          },
-        })
-        .then((res) => {
-          setLoading(false);
-          setResponse(true);
-          setForm(initialForm);
-          setTimeout(() => setResponse(false), 5000);
-        });
-    } else {
-      return;
-    }
-  };
+    const emailRegex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i
+	  const ciRegex = /^(\d){4,8}/i
+    const cadenaRegex=/^(\w){1,40}/i
+  const numeroRegex = /^(\d){4,8}/i
+    
+  
 
   return {
     form,
